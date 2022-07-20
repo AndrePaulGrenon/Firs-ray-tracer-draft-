902259a6e91465d50277c67242e72b4ec9b4ed9e
André-Paul <88338108+AndrePaulGrenon@users.noreply.github.com>
2022-07-20 14:11:06 -0400

    Update ft_pixel_put.c

--- src/ft_pixel_put.c
+++ src/ft_pixel_put.c
@@ -7,3 +7,28 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
+
+
+//Gradient : For full raimbow color, style = 8)
+t_data	*ft_gradient(int w, int h, int style, t_data *img)
+{
+	int 	i;
+	int 	j;
+	int	hexa;
+
+	i = 0;
+	j = 0;
+	while(i < w)
+	{
+		j = 0;
+		while (j < h)
+		{
+			hexa = 0x00000000;
+			hexa = (i*255)/w+((((w-i)*255)/w)<<(style * 2)+(((j*255)/h)<<style);
+			my_mlx_pixel_put(img,  i, j, hexa);
+			j++;
+		}
+		i++;
+	}
+	return (img);
+}
